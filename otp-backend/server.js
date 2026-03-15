52e963cd0bb6c751edd59c1153cd3adaa17a3356
Anil <143868344+ANILKUMAR1521@users.noreply.github.com>
2026-03-15 22:11:00 +0530

    Update server.js

--- otp-backend/server.js
+++ otp-backend/server.js
@@ -7,7 +7,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+//const PORT = 5000;
 
 // 🔑 Replace with your Fast2SMS API Key
 const FAST2SMS_API_KEY = "YOUR_NEW_FAST2SMS_API_KEY";
